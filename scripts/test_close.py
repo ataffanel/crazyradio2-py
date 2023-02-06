24af51883d5e1d34440babdc7094bc027ffe9396
Arnaud Taffanel <arnaud@bitcraze.io>
2023-02-06 14:15:01 +0100

    Add method to close the radio

---
+++ scripts/test_close.py
@@ -0,0 +1,10 @@
+import crazyradio2
+
+
+radio = crazyradio2.Crazyradio2()
+
+radio.close()
+
+radio = crazyradio2.Crazyradio2()
+
+print("Pass")
--- src/lib.rs
+++ src/lib.rs
@@ -106,19 +106,17 @@ impl Crazyradio2 {
         self.radio.esb_send_packet(channel, address, data)?;
         Ok(())
     }
-}
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+    pub fn close(&self) -> anyhow::Result<()> {
+        self.radio.close();
+        Ok(())
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 #[pyo3(name="crazyradio2")]
 fn crazyradio2_module(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_class::<Crazyradio2>()?;
 
     Ok(())
